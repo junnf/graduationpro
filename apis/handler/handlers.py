eb4f71858f79eae5f19ff709978ceb62ff1563b5
junn <junnflow@gmail.com>
2016-04-21 19:28:46 +0800

    m

--- apis/handler/handlers.py
+++ apis/handler/handlers.py
@@ -77,7 +77,6 @@ class LoginHandler(BaseHandler):
         _pass = self.get_argument("password")
         try:
             _get = self.db.query("SELECT passwd FROM user_student WHERE name = '{}';".format(_user))
-	    print _get
             _passmd5 = self.db.query("SELECT MD5('{}');".format(_pass))
             if _get == []:
                 self.write(json.dumps({"code":3,"information":"不存在该用户"}))
